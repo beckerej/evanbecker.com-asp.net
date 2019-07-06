e9548af68505563f679753c3fa8a77936e956c61
Evan Becker <ebecker.designs@gmail.com>
2019-07-06 11:21:24 -0700

    cert changes

--- EvanBecker/Program.cs
+++ EvanBecker/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -15,7 +16,14 @@ namespace EvanBecker
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
-                .UseKestrel()
+                .UseKestrel(options =>
+                {
+                    options.Listen(IPAddress.Any, 80);
+                    options.Listen(IPAddress.Any, 443, listenOptions =>
+                    {
+                        listenOptions.UseHttps("www.evanbecker.com.pfx", Environment.GetEnvironmentVariable("MYSECRET_PASS"));
+                    });
+                })
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseUrls("http://*:80", "https://*:443")
                 .UseSetting("https_port", "443")
